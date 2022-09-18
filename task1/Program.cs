13a3ed546f008b7fc2bda57e7e8129162c8d34ff
Ivan <markelovv1999@yandex.ru>
2022-09-18 21:05:58 +0300

    do part of task(add fill and print arr(string)

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Привет, введи количество элементов в массиве:");
+
+int a = Convert.ToInt32(Console.ReadLine());
+string[] NewArr = new string[a];
+
+
+
+void FillArray(string[] mass) 
+{
+    int length = mass.Length;
+    {
+        for (int i = 0; i < length; i++)
+        {
+            mass[i] = Console.ReadLine();
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    int length = array.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (i< length-1)
+        {
+            Console.Write($"[{array[i]}], ");
+        }
+        else 
+        {
+            Console.Write($"[{array[i]}]");
+        }
+        
+    }
+}
+
+FillArray(NewArr);
+PrintArray(NewArr);
+
