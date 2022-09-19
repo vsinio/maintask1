02d80084fc79eaa98bb52c8b409b098c3783bde7
Ivan <markelovv1999@yandex.ru>
2022-09-19 14:14:16 +0300

    do some code in somecode =)

--- somecode/Program.cs
+++ somecode/Program.cs
@@ -1,1 +1,20 @@
 ﻿// hello, test fold
+int [] arr = new int[5];
+
+int checkqty(int[] array)
+{
+    int lengtharr = array.Length;
+    int count = 0;
+    for (int i = 0; i < lengtharr; i++)
+    {
+        int strLen = array[i].Length;
+        if (strLen <= 3)
+        {
+            count++;
+        }
+    return count;
+    }
+
+}
+
+Console.Write(checkqty(arr));
